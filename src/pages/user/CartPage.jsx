e4411414384311f6f1206bf090e6e10fa76c4a48
Luja Chitrakar <lujachitrakar156@gmail.com>
2024-11-21 16:14:23 +0545

    adding checkoutpage

--- src/pages/user/CartPage.jsx
+++ src/pages/user/CartPage.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import { useCart } from "../../contexts/CartContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 const CartPage = () => {
   const { cartItems, updateItemCount, removeFromCart } = useCart();
+  const navigate = useNavigate();
+  const handleCheckout = () => {
+    navigate("/checkout");
+  };
 
   return (
     <div className="w-full min-h-screen px-16">
@@ -58,7 +63,10 @@ const CartPage = () => {
             </span>
           </div>
           <span>*Shipping Calculated at checkout*</span>
-          <button className="bg-blue-500 px-10 py-2 rounded-md">
+          <button
+            className="bg-blue-500 px-10 py-2 rounded-md"
+            onClick={handleCheckout}
+          >
             Proceed to Checkout
           </button>
         </div>
